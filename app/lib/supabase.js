e660e0e93656e3565ff1b9b41d716db2f6d8d77c
adraintseee <adriantseee2@gmail.com>
2025-05-20 18:16:50 +0800

    testing 2

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single Supabase client for interacting with your database
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
 export const supabase = createClient(
   process.env.SUPABASE_URL || '',
   process.env.SUPABASE_ANON_KEY || ''
